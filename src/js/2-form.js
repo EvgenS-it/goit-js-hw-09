ec908c5ba3eb69dbfa70c60e8de8492166da8c6a
EvgenS-it <evgens.it101@gmail.com>
2024-04-28 19:45:14 +0300

    form finished

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,49 @@
 const formData = {
-    email: "",
-    message: "",
-} 
+  email: "",
+  message: "",
+};
+
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+
+populateFormFields();
+
+form.addEventListener("input", (evt) => {
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  if (form.elements.email.value === "" || form.elements.message.value === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
+
+function populateFormFields() {
+  const dataFromLs = localStorage.getItem(localStorageKey);
+
+  if (dataFromLs) {
+    const dataFromLsParse = JSON.parse(dataFromLs);
+    form.elements.email.value = dataFromLsParse.email;
+    form.elements.message.value = dataFromLsParse.message;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
